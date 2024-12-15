1c0fd2fd0b7709bac1bbe216af7cc6f144a2325d
require-s <requiresignup@hotmail.com>
2024-12-15 09:06:52 +0000

    static webpage

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,23 @@
-fn main() {
-    println!("Hello, world!");
+use axum::{routing::get, Router};
+use maud::{html, Markup};
+use std::io;
+use tokio::net::TcpListener;
+
+// Create a simple route handler function
+async fn hello_world() -> Markup {
+    html! {
+        h1 { "yoo" }
+    }
+}
+
+#[tokio::main]
+async fn main() -> io::Result<()> {
+    let app = Router::new().route("/", get(hello_world));
+
+    // Start the server
+    let listener = TcpListener::bind("0.0.0.0:3000").await?;
+    axum::serve(listener, app).await?;
+
+
+    Ok(())
 }
